e344ac331f32d75daae5443cbd0dfb68272ca3ea
xia0m <alexma325@gmail.com>
2020-02-08 09:08:00 -0800

    added cloudformation

--- Cloudformation/create_eks_vpc.sh
+++ Cloudformation/create_eks_vpc.sh
@@ -1,4 +1,4 @@
-aws cloudformation create-stack --stack-name eks-vpc \
+aws cloudformation create-stack --stack-name eks-kub \
 --template-body file://eks-vpc.yaml \
 --parameters file://eks-vpc-params.json \
 --capabilities CAPABILITY_NAMED_IAM
